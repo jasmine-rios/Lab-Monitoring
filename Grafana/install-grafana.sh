ea97873a57e90f4c74902f6c92b47fc686518b43
Jasmine Rios <93607592+jasmine-rios@users.noreply.github.com>
2024-02-29 17:01:34 -0500

    Custom Script having formatting issues. install-grafana.sh works but only if I run it manually. UI works.

--- Grafana/install-grafana.sh
+++ Grafana/install-grafana.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
-# This installs Grafana from the RPM but allows you to control the version of Grafana.
-# Manually updating Grafana is required by the script.
+# Check if Grafana is already installed and running
+if systemctl is-active --quiet grafana-server; then
+    echo "Grafana is already installed and running."
+    echo "To access the Grafana UI, use the following URL in a supported browser:"
+    echo "http://<PublicIp>:3000"
+    echo "Use 'admin' as the username and password then reset the password"
+else
+    # Install Grafana using Yum
+    sudo yum install -y https://dl.grafana.com/enterprise/release/grafana-enterprise-10.3.3-1.x86_64.rpm # Latest package, change if not latest
 
-# Install Grafana using Yum
+    # Enable Grafana to be able to start
+    sudo systemctl enable grafana-server
 
-sudo yum install -y https://dl.grafana.com/enterprise/release/grafana-enterprise-10.3.3-1.x86_64.rpm # Latest package
+    # Reload the daemon
+    sudo systemctl daemon-reload
 
-# Enable Grafana to be able to start
+    # Enable VM firewall to allow port 3000
+    sudo firewall-cmd --zone=public --add-port=3000/tcp --permanent
 
-sudo systemctl enable grafana-server
+    # Start Grafana
+    sudo systemctl start grafana-server
 
-# Reload the daemon
+    # See if Grafana is running
+    sudo systemctl status grafana-server
 
-sudo systemctl daemon-reload
-
-# Start Grafana
-
-sudo systemctl start grafana-server
-
-# See if Grafana is running
-
-sudo systemctl status grafana-server
-
-      # Check if Grafana is running
-      if sudo systemctl is-active --quiet grafana-server; then
-          echo "Grafana is set up and ready to use."
-          echo "To access the Grafana UI, use the following URL in a supported browser:"
-          echo "http://${MyEC2Instance.PublicIp}:3000"
-      else
-          echo "Grafana setup encountered an issue. Check Grafana service status using: sudo systemctl status grafana-server"
-      fi
+    # Check if Grafana is running
+    if systemctl is-active --quiet grafana-server; then
+        echo "Grafana is set up and ready to use."
+        echo "To access the Grafana UI, use the following URL in a supported browser:"
+        echo "http://<PublicIp>:3000"
+        echo "Use 'admin' as the username and password then reset the password"
+    else
+        echo "Grafana setup encountered an issue. Check Grafana service status using: sudo systemctl status grafana-server"
+    fi
+fi
